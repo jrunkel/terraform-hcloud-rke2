b7378c263cf371abf8576e1f0790146f9078d1fd
Jörg Runkel <jrunkel@codevise.de>
2025-07-11 19:53:58 +0200

    add option to enable useproxy

--- cluster-ingresscontroller.tf
+++ cluster-ingresscontroller.tf
@@ -5,7 +5,7 @@ resource "kubectl_manifest" "ingress_configuration" {
 }
 
 resource "kubectl_manifest" "ingress_use_proxy" {
-  depends_on = [hcloud_load_balancer_service.web_lb_k8s_service]
+  depends_on = [hcloud_load_balancer_service.management_lb_k8s_service]
   count      = var.enable_nginx_useproxy ? 1 : 0
   yaml_body  = file("${path.module}/templates/values/ingress_controller_proxy.yaml")
 }
